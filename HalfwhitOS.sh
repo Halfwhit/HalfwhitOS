9f6ba8f4f1df9a3fe357bb990b72290dc1dd4e0b
Halfwhit <Halfwhit@gmail.com>
2023-01-09 15:15:51 +0000

    Update HalfwhitOS.sh
    Added devtools

--- HalfwhitOS.sh
+++ HalfwhitOS.sh
@@ -16,7 +16,7 @@ while true; do
 done
 
 # Setup lemurs and qtile (+configs)
-paru -S xsel xclip lemurs-git qtile pacwall-git hsetroot alacritty python-setuptools pycritty
+paru -S xsel xclip lemurs-git qtile pacwall-git hsetroot alacritty python-setuptools pycritty devtools
 sudo mkdir /etc/lemurs/wms; sudo cp ./etc-configs/lemurs-qtile /etc/lemurs/wms/qtile 
 sudo systemctl enable lemurs.service
 # configs
